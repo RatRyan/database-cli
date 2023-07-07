eb365e7ce3f6a71be59dce5fec0305d0211aaa2a
RatRyan <ryantratajczak@gmail.com>
2023-07-07 14:20:22 -0600

    initialized commands

---
+++ cmd/employee.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// employeeCmd represents the employee command
+var employeeCmd = &cobra.Command{
+	Use:   "employee",
+	Short: "provides tools for managing employees",
+	Long: ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("employee called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(employeeCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// employeeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// employeeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2023 Ryan Ratajczak ryantratajczak@gmail.com
 
 */
 package cmd
@@ -10,21 +10,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "datacli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "CLI tool for managing .txt database",
+	Long: ``,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
