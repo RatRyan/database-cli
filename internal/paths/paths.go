efa6d4c1d33f0602022b293589eb168210e212f7
RatRyan <ryantratajczak@gmail.com>
2023-07-08 23:07:09 -0600

    finished restructure

--- internal/paths/paths.go
+++
@@ -1,5 +0,0 @@
-package paths
-
-var People = "C:/Users/rratajczak/Documents/Neumont/2022-2023/Q4/DBT230/people/"
-var Long = "C:/Users/rratajczak/Documents/Neumont/2022-2023/Q4/DBT230/people/long/"
-var LongSer = "C:/Users/rratajczak/Documents/Neumont/2022-2023/Q4/DBT230/people/long serialized/"
