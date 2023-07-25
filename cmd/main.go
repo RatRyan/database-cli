9eb2b894c657ab2fc9a3becfb522009af46c1301
RatRyan <ryantratajczak@gmail.com>
2023-07-24 23:03:43 -0600

    added read, update, and delete functions

--- cmd/main.go
+++ cmd/main.go
@@ -2,56 +2,81 @@ package main
 
 import (
 	"context"
-	"time"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
 
-	"github.com/RatRyan/dbapp/internal/record/restaurant"
+	"github.com/RatRyan/dbapp/internal/record/employee"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const PATH = "C:/Users/rratajczak/Documents/people/long/"
+
 var clientOptions = options.Client().ApplyURI("mongodb://localhost:2717")
 var client, err = mongo.Connect(context.Background(), clientOptions)
+var database = client.Database("peopledb")
+var collection = database.Collection("people")
 
-var database = client.Database("restaurantdb")
-var collection = database.Collection("restaurants")
-
-func CreateRecord(record interface{}, collectionName string) {
-	collection = database.Collection(collectionName)
+func CreateRecord(record interface{}) {
 	collection.InsertOne(context.TODO(), record)
 }
 
-func readRecord() {
-
+func readRecord(id int) {
+	var result employee.Employee
+	filter := bson.D{{Key: "id", Value: id}}
+	collection.FindOne(context.TODO(), filter).Decode(&result)
+	fmt.Println(result)
 }
 
-func updateRecord() {
-
+func updateRecord(id int) {
+	filter := bson.D{{Key: "id", Value: id}}
+	update := bson.D{{Key: "$set", Value: bson.D{{Key: "first_name", Value: "Chris Wilson"}}}}
+	_, err := collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func deleteRecord() {
+func deleteRecord(id int) {
+	filter := bson.D{{Key: "id", Value: id}}
+	_, err := collection.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		panic(err)
+	}
+}
 
+func initRecords() {
+	dir, _ := os.ReadDir(PATH)
+
+	var wg sync.WaitGroup
+	for _, file := range dir {
+		wg.Add(1)
+		go func(entry os.DirEntry) {
+			data, _ := os.ReadFile(PATH + entry.Name())
+			employeeInfo := strings.Split(string(data), ", ")
+
+			id, _ := strconv.Atoi(employeeInfo[0])
+			hireDate, _ := strconv.Atoi(strings.TrimSuffix(employeeInfo[3], "\r\n"))
+
+			record := employee.Employee{
+				Id:        id,
+				FirstName: employeeInfo[1],
+				LastName:  employeeInfo[2],
+				HireDate:  hireDate,
+			}
+			CreateRecord(record)
+			wg.Done()
+		}(file)
+		wg.Wait()
+	}
 }
 
 func main() {
 	defer client.Disconnect(context.Background())
 
-	record := restaurant.Restaurant{
-		Address: restaurant.AddressInfo{
-			Building: "test building",
-			Coord:    []float64{0.12, 3.45},
-			Street:   "test street",
-			Zipcode:  "test zipcode",
-		},
-		Borough: "test borough",
-		Cuisine: "test cuisine",
-		Grades: []restaurant.GradeInfo{{
-			Date:  time.Now(),
-			Grade: "A",
-			Score: 5,
-		}},
-		Name:         "test name",
-		RestaurantId: "test restaurant_id",
-	}
-
-	CreateRecord(record, "restaurants")
+	deleteRecord(1)
 }
--- internal/record/employee/employee.go
+++ internal/record/employee/employee.go
@@ -1,8 +1,8 @@
 package employee
 
 type Employee struct {
-	Id        int
-	FirstName string
-	LastName  string
-	HireDate  int
+	Id        int    `bson:"id"`
+	FirstName string `bson:"first_name"`
+	LastName  string `bson:"last_name"`
+	HireDate  int    `bson:"hire_date"`
 }
